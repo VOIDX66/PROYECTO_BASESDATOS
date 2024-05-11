9dfb4ae114db2992f7395b01426646f495adceec
Jaider Rendon <jaiderrendon66@gmail.com>
2024-05-11 17:00:04 -0500

    Implementación de filtrado de caracteres
    Por medio de expresiones regulares filtramos los caracteres indeseados en los campos de nombre y contraseña

--- PROYECTO DB/js/verificarCaracteres.js
+++ PROYECTO DB/js/verificarCaracteres.js
@@ -1,10 +1,30 @@
-function filtrarCaracteresEspeciales(input) {
-    // Expresión regular para permitir solo letras, números y espacios
-    const regex = /^[a-zA-Z0-9_]+$/;
-    const valor = input.value;
+//Funcion para filtrar los caracteres especiales
+function filtrarCaracteresEspeciales() {
+    const regex_user = /^[a-zA-Z0-9_]+$/;
+    const inputUserElement = document.getElementById('username');
+    const valor = inputUserElement.value;
 
-    if (!regex.test(valor)) {
-        input.value = valor.replace(/[^a-zA-Z0-9\s]/g, '');
+    if (!regex_user.test(valor)) {
+        inputUserElement.value = valor.replace(/[^a-zA-Z0-9_]/g, '');
         alert('No se permiten caracteres especiales');
     }
+   
+    const regex_email = /^[a-zA-Z0-9_-]+@[a-zA-Z0-9.]+$/;
+    const inputEmailElement = document.getElementById('email');
+    const valorEmail = inputEmailElement.value;
+
+    if (!regex_email.test(valorEmail)) {
+        inputEmailElement.value = ''; // Vaciar el campo si el correo no es válido
+        alert('Por favor, introduzca una dirección de correo electrónico válida');
+    }
+
+    const regex_password = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{4,}$/;
+    const inputPasswordElement = document.getElementById('passw');
+    const valorPassword = inputPasswordElement.value;
+
+    if (!regex_password.test(valorPassword)){
+        inputPasswordElement.value = ''; // Vaciar el campo si la contraseña no es válida
+        alert("La contraseña no debe contener caracteres especiales, esta debe tener minimo 4 digitos y contener"
+              +"al menos una mayuscula y minuscula");
+    }    
 }
